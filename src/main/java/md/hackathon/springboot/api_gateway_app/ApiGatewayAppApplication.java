4d1982346d213b38b4738da1b58a7a7097ac2b20
rodio <busurmanov.rodion@gmail.com>
2023-06-25 15:59:06 +0300

    Init.

---
+++ src/main/java/md/hackathon/springboot/api_gateway_app/ApiGatewayAppApplication.java
@@ -0,0 +1,13 @@
+package md.hackathon.springboot.api_gateway_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiGatewayAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApiGatewayAppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/md/hackathon/springboot/api_gateway_app/ApiGatewayAppApplicationTests.java
@@ -0,0 +1,13 @@
+package md.hackathon.springboot.api_gateway_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiGatewayAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
